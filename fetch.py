1396bdc6ae3656eab69df2f00e96e95544fc87f2
khashayar <khashayar.k.m@gmail.com>
2020-10-11 20:50:39 +0330

    added pagination, improved input

--- fetch.py
+++ fetch.py
@@ -2,13 +2,37 @@ import os
 import sys
 import googleapiclient.discovery
 
-def main():
 
+def main():
     url = sys.argv[1]
+    try:
+        fetch_count = int(sys.argv[2])
+    except IndexError:
+        fetch_count = 500
+
+
+    perPage = 100
 
     videoId = url.split('v=')[1].split('&')[0]
 
+    nextPageToken = 'initial'
 
+    allComments = list()
+    comments = list()
+    times = fetch_count/perPage
+
+    i = 0
+
+    while nextPageToken and i < times:
+        comments, nextPageToken = getComments(
+            perPage, videoId, nextPage=nextPageToken)
+        allComments.extend(comments)
+        i = i + 1
+
+    print("\n\n ".join(allComments))
+
+
+def getComments(perPage, videoId, nextPage=None):
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
     api_service_name = "youtube"
@@ -16,23 +40,36 @@ def main():
     DEVELOPER_KEY = "AIzaSyCMV0mcZa5eKnKVTu7C14wYW8iPIwNbX_g"
 
     youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, developerKey = DEVELOPER_KEY)
+        api_service_name, api_version, developerKey=DEVELOPER_KEY)
 
-    request = youtube.commentThreads().list(
-        part="snippet",
-        videoId=videoId,
-        maxResults=200
-    )
+    if nextPage and nextPage != 'initial':
+        request = youtube.commentThreads().list(
+            part="snippet",
+            videoId=videoId,
+            maxResults=perPage,
+            pageToken=nextPage
+        )
+    else:
+        request = youtube.commentThreads().list(
+            part="snippet",
+            videoId=videoId,
+            maxResults=perPage,
+        )
+
     response = request.execute()
+
+    if response.get('nextPageToken') is None:
+        nextPageToken = None
+    else:
+        nextPageToken = response['nextPageToken']
 
     comments = list()
     for item in response['items']:
-        comments.append(item['snippet']['topLevelComment']['snippet']['textOriginal'] )
+        comments.append(item['snippet']['topLevelComment']
+                        ['snippet']['textOriginal'])
 
-    print ("\n\n ".join(comments))
+    return comments, nextPageToken
+
 
 if __name__ == "__main__":
     main()
-
-
-    
