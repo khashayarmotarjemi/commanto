5ca7a6f07ccf486723ff4b4084972c2823fe4e58
khashayar <khashayar.k.m@gmail.com>
2020-10-15 17:25:21 +0330

    fixed default fetch count

--- fetch.py
+++ fetch.py
@@ -1,38 +1,64 @@
 import os
 import sys
 import googleapiclient.discovery
+from subprocess import run, Popen, PIPE, STDOUT
+from rake_nltk import Rake
 
 
 def main():
     url = sys.argv[1]
-    try:
-        fetch_count = int(sys.argv[2])
-    except IndexError:
-        fetch_count = 500
+    fetch_count = int(sys.argv[2])
+
+    video_id = url.split('v=')[1].split('&')[0]
+
+    comments = get_all_comments(video_id, fetch_count)
+
+    c = input('what action? (f: fzf, w: words): ')
+
+    if c == 'f':
+        comments_str = "\n\n".join(comments)
+
+        fzf_p = run(['fzf'], stdout=PIPE,
+                    input=comments_str, encoding='utf-8')
+        print(fzf_p.stdout)
+
+    elif c == 'w':
+        # Uses stopwords for english from NLTK, and all puntuation characters.
+        print(get_key_words(comments))
 
 
-    perPage = 100
+def get_key_words(comments):
+    r = Rake()
+    r.extract_keywords_from_sentences(comments)
+    return r.get_ranked_phrases_with_scores()
 
-    videoId = url.split('v=')[1].split('&')[0]
 
-    nextPageToken = 'initial'
+def get_all_comments(video_id, fetch_count):
+    per_page = 20
 
-    allComments = list()
+    if per_page > fetch_count:
+        per_page = fetch_count
+
+    if per_page > fetch_count:
+        per_page = fetch_count
+
+    next_page_token = 'initial'
+
+    all_comments = list()
     comments = list()
-    times = fetch_count/perPage
-
+    times = fetch_count/per_page
     i = 0
 
-    while nextPageToken and i < times:
-        comments, nextPageToken = getComments(
-            perPage, videoId, nextPage=nextPageToken)
-        allComments.extend(comments)
+    while next_page_token and i < times:
+        comments, next_page_token = get_comments(
+            per_page, video_id, next_page=next_page_token)
+        all_comments.extend(comments)
         i = i + 1
 
-    print("\n\n ".join(allComments))
+    return all_comments
 
 
-def getComments(perPage, videoId, nextPage=None):
+def get_comments(per_page, video_id, next_page=None):
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
     api_service_name = "youtube"
@@ -42,18 +68,18 @@ def getComments(perPage, videoId, nextPage=None):
     youtube = googleapiclient.discovery.build(
         api_service_name, api_version, developerKey=DEVELOPER_KEY)
 
-    if nextPage and nextPage != 'initial':
+    if next_page and next_page != 'initial':
         request = youtube.commentThreads().list(
             part="snippet",
-            videoId=videoId,
-            maxResults=perPage,
-            pageToken=nextPage
+            videoId=video_id,
+            maxResults=per_page,
+            pageToken=next_page
         )
     else:
         request = youtube.commentThreads().list(
             part="snippet",
-            videoId=videoId,
-            maxResults=perPage,
+            videoId=video_id,
+            maxResults=per_page,
         )
 
     response = request.execute()
